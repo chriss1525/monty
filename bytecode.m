a5bab64b3adb9dc40626d9c56e6b0d3b1222173f
chriss1525 <okothchris15@gmail.com>
2023-03-24 12:33:27 +0300

    fix error messages

--- bytecode.m
+++ bytecode.m
@@ -1,4 +1,9 @@
-push 1$
-push 2$
-push 
-pall$
+pall
+push 1
+pall
+push 2
+pall
+push 3
+pall
+push 4
+pall
--- get_instructions.c
+++ get_instructions.c
@@ -14,18 +14,18 @@ void (*opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{NULL, NULL}};
 	int i;
 
-	printf("Processing opcode: %s\n", opc);
+	/*printf("Processing opcode: %s\n", opc);*/
 	data = 0;
 	for (i = 0; instruct[i].opcode; i++)
 	{
 		if (strcmp(instruct[i].opcode, opc) == 0)
 		{
 			data++;
-			printf("Selected function: %p\n", (void *)instruct[i].f);
+			/*printf("Selected function: %p\n", (void *)instruct[i].f);*/
 			return (instruct[i].f);
 		}
 	}
 
-	printf("Error: Unknown instruction %s\n", opc);
+	/*printf("Error: Unknown instruction %s\n", opc);*/
 	exit(EXIT_FAILURE);
 }
